bf7eaeafdbb3de0f9f37520ad624163db94b047f
Jake <jakeb_93@hotmail.com>
2013-03-02 00:27:37 -0500

    player updated, and nav hover

--- js/scripts.js
+++ js/scripts.js
@@ -12,9 +12,17 @@ $(document).ready(function(){
 		return false;
 	});
 
+	//list toggle
 	$('.list').click(function(){
 		$(this).toggleClass('listselect');
 		$('.tracklist').toggle(400);
+	});
+
+	//track indicator
+	// try to use addclass here
+	$('.tracklist p').click(function(){
+		$('.tracklist p').not(this).css("border-left", "none");
+		$(this).css("border-left", "#d06200 solid 8px");
 	});
 
 });
